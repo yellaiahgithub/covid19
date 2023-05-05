09bb95952a99678d4b89796bd08f4aa5c5f7c1c6
yellaiahgithub <105124131+yellaiahgithub@users.noreply.github.com>
2023-05-05 19:47:34 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/server/src
-npm start
-pm2 start npm --name "covidapp" -- start
-pm2 startup
-pm2 save
-pm2 restart all
+cd /home/ec2-user/server/dist
+cp -r /home/ec2-user/server/dist/* /var/www/html/
+systemctl restart nginx
