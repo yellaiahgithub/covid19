1436e271c40f70085781f74908f9968283df5058
yellaiahgithub <105124131+yellaiahgithub@users.noreply.github.com>
2023-05-05 12:28:08 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/server
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+curl -fsSL https://rpm.nodesource.com/setup_14.x | sudo bash -
 yum -y install nodejs npm
